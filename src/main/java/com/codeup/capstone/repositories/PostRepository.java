d44c82ceccfc71902f0a067fc6d2413dde1b1d81
Binjita Kandel Baral <binjita.baral@gmail.com>
2020-10-23 19:28:07 -0500

    deleted method from  repo

--- src/main/java/com/codeup/capstone/repositories/PostRepository.java
+++ src/main/java/com/codeup/capstone/repositories/PostRepository.java
@@ -6,7 +6,4 @@ import org.springframework.data.jpa.repository.Query;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
-    @Query("from Post a where a.id = ?1")
-    Post getPostById(long id);
-
 }
