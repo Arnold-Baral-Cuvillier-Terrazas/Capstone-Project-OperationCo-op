1761a1c2d67c268b27dbb06248aebab3675f4d2b
Binjita Kandel Baral <binjita.baral@gmail.com>
2020-10-25 22:06:00 -0500

    added tag and usertags class for descriptive tag purpose

--- src/main/java/com/codeup/capstone/models/Tag.java
+++ src/main/java/com/codeup/capstone/models/Tag.java
@@ -1,4 +1,43 @@
 package com.codeup.capstone.models;
 
+import javax.persistence.*;
+
+
+@Entity
+@Table(name="tag")
 public class Tag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false, length = 100,unique = true)
+    private String name;
+
+// ---------------- Constructor
+
+    public Tag(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Tag() {
+    }
+
+//----------------getters and setters
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
