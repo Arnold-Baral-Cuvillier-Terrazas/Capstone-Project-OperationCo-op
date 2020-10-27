cab8d7c3b6975b1403b02dd90839908746a358be
Amaro Terrazas <42896111+AmaroT@users.noreply.github.com>
2020-10-27 15:44:16 -0500

    removal attempt

--- src/main/resources/static/js/userProfile.js
+++ src/main/resources/static/js/userProfile.js
@@ -21,32 +21,32 @@
 // //     }
 // // };
 //
-// const options = {
-//     onUploadDone : updateImage ,
-//     accept: 'image/*',
-//     maxSize: 10* 1000* 1000,
-//     uploadInBackground: false
-// }
-//
-// function updateImage(result){
-//     const filedata = result.filesUploaded[0];
-//     console.log(filedata);
-//
-//     $("#profileUrl").val(filedata.url);
-//
-// }
-//
-// // client.picker().open();
-//
-//
-// $(document).ready(function() {
-//
-//     //filestack
-//     console.log(url);
-//     $("#profile-btn").click(function(){
-//         stackClient.picker(options).open();
-//     });
-//
-//
-//
-// })
+const options = {
+    onUploadDone : updateImage ,
+    accept: 'image/*',
+    maxSize: 10* 1000* 1000,
+    uploadInBackground: false
+}
+
+function updateImage(result){
+    const filedata = result.filesUploaded[0];
+    console.log(filedata);
+
+    $("#profileUrl").val(filedata.url);
+
+}
+
+// client.picker().open();
+
+
+$(document).ready(function() {
+
+    //filestack
+    console.log(url);
+    $("#profile-btn").click(function(){
+        stackClient.picker(options).open();
+    });
+
+
+
+})
