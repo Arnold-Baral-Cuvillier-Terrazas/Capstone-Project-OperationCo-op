265629a6068e0889a86bc03e0682469e9d2054f9
Ryan Cuvillier <cuvillier.ryan@gmail.com>
2020-10-27 14:13:57 -0500

    groupUserFixed

--- src/main/resources/static/js/userProfile.js
+++ src/main/resources/static/js/userProfile.js
@@ -1,52 +1,52 @@
-
-//starting functionality for profile page
-
-// const options = {
-//     onFileUploadFinished: file => {
-//         // If you throw any error in this function it will reject the file selection.
-//         // The error message will be displayed to the user as an alert.
-//         if (file.size > 1000 * 1000) {
-//             throw new Error('File too big, select something smaller than 1MB');
-//         }
-//         // console.log(file.url);
 //
-//         //fetch request
-//         fetch(url , {
-//             method: 'POST',
-//             headers: {
-//                 'Content-Type': 'application/x-www-form-urlencoded',
-//             },
-//             body: $.param({userId: userId, url: file.url})
-//         });
-//     }
-// };
-
-const options = {
-    onUploadDone : updateImage ,
-    accept: 'image/*',
-    maxSize: 10* 1000* 1000,
-    uploadInBackground: false
-}
-
-function updateImage(result){
-    const filedata = result.filesUploaded[0];
-    console.log(filedata);
-
-    $("#profileUrl").val(filedata.url);
-
-}
-
-// client.picker().open();
-
-
-$(document).ready(function() {
-
-    //filestack
-    console.log(url);
-    $("#profile-btn").click(function(){
-        stackClient.picker(options).open();
-    });
-
-
-
-})
+// //starting functionality for profile page
+//
+// // const options = {
+// //     onFileUploadFinished: file => {
+// //         // If you throw any error in this function it will reject the file selection.
+// //         // The error message will be displayed to the user as an alert.
+// //         if (file.size > 1000 * 1000) {
+// //             throw new Error('File too big, select something smaller than 1MB');
+// //         }
+// //         // console.log(file.url);
+// //
+// //         //fetch request
+// //         fetch(url , {
+// //             method: 'POST',
+// //             headers: {
+// //                 'Content-Type': 'application/x-www-form-urlencoded',
+// //             },
+// //             body: $.param({userId: userId, url: file.url})
+// //         });
+// //     }
+// // };
+//
+// const options = {
+//     onUploadDone : updateImage ,
+//     accept: 'image/*',
+//     maxSize: 10* 1000* 1000,
+//     uploadInBackground: false
+// }
+//
+// function updateImage(result){
+//     const filedata = result.filesUploaded[0];
+//     console.log(filedata);
+//
+//     $("#profileUrl").val(filedata.url);
+//
+// }
+//
+// // client.picker().open();
+//
+//
+// $(document).ready(function() {
+//
+//     //filestack
+//     console.log(url);
+//     $("#profile-btn").click(function(){
+//         stackClient.picker(options).open();
+//     });
+//
+//
+//
+// })
