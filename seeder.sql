42419b74d580c069d56403dc74dd669625f46a1a
Ryan Cuvillier <cuvillier.ryan@gmail.com>
2020-10-29 13:41:35 -0500

    groupAdminCommentedOUt

--- seeder.sql
+++ seeder.sql
@@ -1,5 +1,4 @@
 
-USE operationcoop_db;
+USE operationCoop_db;
 INSERT INTO user(email, full_name, user_name, password, birth_date)
-VALUES ('bijit@gmail.com', 'Bijit' ,'bijit16', '12345', '01/04/2005 01:01:01'),
-       ('ryan@gmail.com', 'Ryan' ,'ryan', 'ryan', '09/09/2000 01:01:01');
+VALUES ('bijit@gmail.com', 'Bijit' ,'bijit16', '12345', '01/04/2005');
