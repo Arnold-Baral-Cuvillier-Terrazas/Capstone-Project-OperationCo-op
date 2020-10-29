2e2d0a64bb9d542f21cf2c663f8ab83d4d6b3f31
Amaro Terrazas <42896111+AmaroT@users.noreply.github.com>
2020-10-28 23:14:19 -0500

    api work

--- src/main/resources/static/js/gamescript.js
+++ src/main/resources/static/js/gamescript.js
@@ -40,14 +40,14 @@ const get_covers = (ids) => {
             'Client-ID': CLIENT_ID,
             'Authorization': `Bearer ${BEARER}`,
         },
-        body: `fields *; where id = (${ids}); limit 100;`
+        body: `fields id, url; limit 104;`
     })
 }
 
 (async function () {
     let games = [];
     let cover_urls = [];
-    get_games('fields *; where first_release_date > 1577836800 & multiplayer_modes != null; limit 104;')
+    get_games('fields name, first_release_date, summary, total_rating; where total_rating > 50; limit 104;')
         .then(response => {
             response.json().then(function (json) {
                 games = json.sort((a, b) => (a.id > b.id) ? 1 : ((b.id > a.id) ? -1 : 0))
