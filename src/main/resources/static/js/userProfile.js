ae8191434e683567eaee43bfe3b9c7257f693d92
Binjita Kandel Baral <binjita.baral@gmail.com>
2020-11-04 17:25:59 -0600

    created about us page

--- src/main/resources/static/js/userProfile.js
+++ src/main/resources/static/js/userProfile.js
@@ -1,27 +1,7 @@
 
-//
-// //starting functionality for profile page
-//
-// // const options = {
-// //     onFileUploadFinished: file => {
-// //         // If you throw any error in this function it will reject the file selection.
-// //         // The error message will be displayed to the user as an alert.
-// //         if (file.size > 1000 * 1000) {
-// //             throw new Error('File too big, select something smaller than 1MB');
-// //         }
-// //         // console.log(file.url);
-// //
-// //         //fetch request
-// //         fetch(url , {
-// //             method: 'POST',
-// //             headers: {
-// //                 'Content-Type': 'application/x-www-form-urlencoded',
-// //             },
-// //             body: $.param({userId: userId, url: file.url})
-// //         });
-// //     }
-// // };
-//
+
+//File stack Api
+//------starting functionality for profile page
 const options = {
     onUploadDone : updateImage ,
     accept: 'image/*',
@@ -32,49 +12,28 @@ const options = {
 function updateImage(result){
     const filedata = result.filesUploaded[0];
     console.log(filedata);
+    // $("#profileUrl").val(filedata.url);
+    $("#imgP").attr("src",filedata.url);
 
-    $("#profileUrl").val(filedata.url);
+    let formData = new FormData();
+    formData.append("userId", userId);
+    formData.append("url", filedata.url);
+    fetch("/profile/pic", {
+        method:"POST",
+        body:formData,
+    }).catch(err => { console.log(err) });
 
 }
 
-// client.picker().open();
 
-
+//jquery functionality
 $(document).ready(function() {
-
     //filestack
     console.log(url);
     $("#profile-btn").click(function(){
         stackClient.picker(options).open();
     });
 
-
-
 })
 
-//starting functionality for profile page
 
-
-const options = {
-    onUploadDone: updateImage,
-    accept: 'image/*',
-    maxSize: 10 * 1000 * 1000,
-    uploadInBackground: false
-}
-
-function updateImage(result) {
-    const filedata = result.filesUploaded[0];
-    console.log(filedata);
-    $("#profileUrl").val(filedata.url);
-}
-
-//jquery functcionality
-$(document).ready(function () {
-
-    //filestack
-    console.log(url);
-    $("#profile-btn").click(function () {
-        stackClient.picker(options).open();
-    });
-
-})
