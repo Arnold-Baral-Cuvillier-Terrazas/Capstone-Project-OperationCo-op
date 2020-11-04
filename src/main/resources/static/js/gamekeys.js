13d803f9d30232870cef6e934c9540977bcc9ece
Casey Friday <casey@friday-next.com>
2020-11-04 15:34:05 -0600

    commented out js api keys files to upload to production

---
+++ src/main/resources/static/js/gamekeys.js
@@ -0,0 +1,4 @@
+const CLIENT_ID = '3k7keshje15ppkmvfknzvge864tujx';
+const SECRET_KEY = 'a7qogz2yatoa17t46q9yz5vfa5mr64';
+const BEARER = '89gj2uvu8f0x6vhlyo3fvx02z5cfta';
+const API_URL = 'http://127.0.0.1:8010/https://api.igdb.com/v4';
