f368f5a4becdcaba86ba7955578e2097332daca0
Ryan Cuvillier <cuvillier.ryan@gmail.com>
2020-11-06 13:56:18 -0600

    update

---
+++ groupseeder.sql
@@ -0,0 +1,18 @@
+USE operationcoop_db;
+
+INSERT INTO tags (name)
+VALUES
+('foodie'),
+('Cooking'),
+('Animals'),
+('Plants'),
+('Vegan'),
+('non-veg'),
+('Art'),
+('DIY'),
+('Action'),
+('Coder'),
+('Dance'),
+('Music'),
+('acting'),
+('traveling');
