3afbc16a9c51ac8900f56d01c090fca9d8acfc16
Binjita Kandel Baral <binjita.baral@gmail.com>
2020-11-08 18:03:22 -0600

    added pictures on background

--- src/main/java/com/codeup/capstone/repositories/PostRepository.java
+++ src/main/java/com/codeup/capstone/repositories/PostRepository.java
@@ -17,8 +17,6 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     @Query("from Post a where a.id = ?1")
     Post getPostById(long id);
 
-
-
 //    This is for posting message on group page
     @Query("from Post a where a.group.id =?1 order by a.date DESC")
  List<Post> mostRecentPostsForGroup (long group_id);
